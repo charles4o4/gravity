2f0497172f2c5d286bc01ab84e01ac45d3e0a96a
charles4o4 <lilianmoretti32@gmail.com>
2024-11-02 08:50:32 +0100

    New Template: React

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',       // Main entry point
-        game: 'src/game.html',     // Additional entry point for game.html
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
