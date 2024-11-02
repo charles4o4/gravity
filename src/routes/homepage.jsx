6ada4a09f488feb58283619934cf8288d02e01b7
charles4o4 <lilianmoretti32@gmail.com>
2024-11-02 15:45:19 +0100

    Made the homepage a bit more responsive, its not perfect but whatever

--- src/routes/homepage.jsx
+++ src/routes/homepage.jsx
@@ -1,5 +1,20 @@
 const Homepage = () => {
-  return <>Welcome to the Homepage.</>;
+  return (
+    <>
+      <div className="homepage-background">
+        <div className="homepage-main">
+          <div className="homepage-header">
+            <h1 id="gravity-h1">GRAVITY</h1>
+            <p id="gravity-p">Learning New Words Made Easier and More Fun!</p>
+          </div>
+        </div>
+        <div className="homepage-footer">
+          <button className="homepage-button" id="homepage-howbtn">HOW TO PLAY</button>
+          <button className="homepage-button" id="homepage-startbtn">START PLAYING</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
