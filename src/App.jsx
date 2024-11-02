8656a26a8f1581574932c5403d60ee131081f827
charles4o4 <lilianmoretti32@gmail.com>
2024-11-02 09:13:26 +0100

    Added the foundation for the new homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Stage1 from './components/Stage1';
 import './App.css'
 
-function App() {
-  return (
-    <>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Stage1 />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
---
+++ src/components/Stage1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Stage1 = () => {
+    return (
+        <div className="background-stage1">
+        </div>
+    );
+};
+
+export default Stage1;
